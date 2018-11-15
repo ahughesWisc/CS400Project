e6230d6ab224327148732c4e5826504d75ae4c2a
ahughesWisc <43834154+ahughesWisc@users.noreply.github.com>
2018-11-15 08:34:13 -0600

    retrying commit
    populating initial files

---
+++ src/application/FoodItemUnitTests.java
@@ -0,0 +1,33 @@
+package application;
+
+/**Filename:	FoodItemUnitTests.java
+* Project:		Final Project (p5)
+* Authors: 		Adam Hughes
+* Emails:		adam.hughes@wisc.edu
+* Course:		CS400
+* Section:		004
+* 
+* Notes:
+*/
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FoodItemUnitTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
