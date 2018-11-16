21bbb799fd5d16a35e7455fe18a84265990ead2f
ahughesWisc <adam.hughes@wisc.edu>
2018-11-15 19:42:04 -0600

    basic unit tests for FoodItem

--- src/application/FoodItemUnitTests.java
+++ src/application/FoodItemUnitTests.java
@@ -11,23 +11,51 @@ package application;
 */
 import static org.junit.Assert.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class FoodItemUnitTests {
 
+	FoodItem foodItem;
+	static final String id = "ID";
+	static final String name = "Name";
+	
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		foodItem = new FoodItem(id, name);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test00000_constructor() {
+		//Exceptions
+		try {
+			foodItem = new FoodItem(null, null);
+		}
+		catch (IllegalArgumentException e) { //correct
+			
+		}
+		try {
+			foodItem = new FoodItem(id, null);
+		}
+		catch (IllegalArgumentException e) { //correct
+			
+		}
+		try {
+			foodItem = new FoodItem(null, name);
+		}
+		catch (IllegalArgumentException e) { //correct
+			
+		}
+	}
+	
+	@Test
+	public void test00001_getID() {
+		assertEquals(id, foodItem.getID());
+	}
+	
+	@Test
+	public void test00010_getName() {
+		assertEquals(name, foodItem.getName());
 	}
 
 }
