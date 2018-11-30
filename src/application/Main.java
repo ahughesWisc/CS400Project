9567f3ef897c56e2dd92769e6b5cad24b58723ea
likea <likea@LAPTOP-3NV16J6C.localdomain>
2018-11-29 18:48:36 -0600

    Removed Border Pane and added Grid Pane

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,6 +20,7 @@ package application;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -27,6 +28,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 
@@ -34,10 +36,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,1400,800);
+		    GridPane root = new GridPane();
+		    root.setHgap(10); // Horizontal gap between rows
+		    root.setVgap(10); // Vertical gap between columns
+		    root.setPadding(new Insets(0, 10, 0, 10)); // Distance between nodes and edges of pane
+			Scene scene = new Scene(root, 1400, 800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.show();
 			
 			// filter line
 			primaryStage.setTitle("Food Program");
