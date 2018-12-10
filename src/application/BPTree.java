479be75648c477e64e986b3a631ed86b07d8def9
ahughes <ahughes@EPIC40020.epic.com>
2018-12-10 09:49:07 -0600

    add reference to incorporate collections

--- src/application/BPTree.java
+++ src/application/BPTree.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
